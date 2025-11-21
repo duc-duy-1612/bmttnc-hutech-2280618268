779033d8c586a8900a9bd66de2ff43e33a792cff
lynki1509 <lynki1509@gmail>
2025-11-21 14:51:44 +0700

    lab01 ex02_02.py

--- lab-01/ex02_02.py
+++ lab-01/ex02_02.py
@@ -1,3 +1,11 @@
 # nhap ban kinh tu nguoi dung 
 
-ban_kinh = float ( input (" nhap ban kinh "))
+ban_kinh = float ( input (" nhap ban kinh cua hinh tron : ") )
+
+# tính diện tích của hình tròn 
+
+dien_tich = 3.14 * (ban_kinh ** 2 )
+
+# in diẹn tích của hình tròn ra mh 
+
+print (" dien tich của hình tròn là : " , dien_tich)
