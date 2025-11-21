5091bf3b3bb0cbd803c4bbe77142d886e847cb43
lynki1509 <lynki1509@gmail>
2025-11-21 14:57:47 +0700

    lab01 ex02_03.py

---
+++ lab-01/ex02_03.py
@@ -0,0 +1,10 @@
+# nhap so từ người dùng 
+
+so = int ( input ("nhap 1 số nguyên : "))
+
+# kiem tra xem so do là chẵn hay lẻ 
+
+if so % 2 == 0 :
+    print (so , "la so chan ")
+else : 
+    print (so , "la so le ")
