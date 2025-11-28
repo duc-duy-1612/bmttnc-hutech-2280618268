aaa5f544ed1e40819aeee453380969abe8415889
lynki1509 <lynki1509@gmail>
2025-11-28 15:57:59 +0700

    [add] lab-02 railfence

--- lab-02/api.py
+++ lab-02/api.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher # <== THÊM IMPORT BÀI 3
 
 app = Flask(__name__)
 
-# CAESAR CIPHER ALGORITHM
+# KHỞI TẠO CÁC ALGORITHM
 caesar_cipher = CaesarCipher()
-
-# VIGENERE CIPHER ALGORITHM
 vigenere_cipher = VigenereCipher()
+railfence_cipher = RailFenceCipher() # <== KHỞI TẠO BÀI 3
 
 # ==================================
 # CAESAR CIPHER ENDPOINTS
@@ -16,10 +16,7 @@ vigenere_cipher = VigenereCipher()
 
 @app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
-    """
-    Mã hóa bản rõ bằng Caesar Cipher.
-    Input: {"plain_text": "...", "key": "..."}
-    """
+    """ Mã hóa bản rõ bằng Caesar Cipher. """
     data = request.json
     plain_text = data['plain_text']
     key = int(data['key'])
@@ -28,10 +25,7 @@ def caesar_encrypt():
 
 @app.route("/api/caesar/decrypt", methods=["POST"])
 def caesar_decrypt():
-    """
-    Giải mã bản mã bằng Caesar Cipher.
-    Input: {"cipher_text": "...", "key": "..."}
-    """
+    """ Giải mã bản mã bằng Caesar Cipher. """
     data = request.json
     cipher_text = data['cipher_text']
     key = int(data['key'])
@@ -44,10 +38,7 @@ def caesar_decrypt():
 
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
-    """
-    Mã hóa bản rõ bằng Vigenère Cipher.
-    Input: {"plain_text": "...", "key": "..."}
-    """
+    """ Mã hóa bản rõ bằng Vigenère Cipher. """
     data = request.json
     plain_text = data['plain_text']
     key = data['key']
@@ -56,14 +47,33 @@ def vigenere_encrypt():
 
 @app.route('/api/vigenere/decrypt', methods=['POST'])
 def vigenere_decrypt():
-    """
-    Giải mã bản mã bằng Vigenère Cipher.
-    Input: {"cipher_text": "...", "key": "..."}
-    """
+    """ Giải mã bản mã bằng Vigenère Cipher. """
     data = request.json
     cipher_text = data['cipher_text']
     key = data['key']
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+# ==================================
+# RAIL FENCE CIPHER ENDPOINTS <== THÊM ĐOẠN CODE NÀY
+# ==================================
+
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def encrypt():
+    """ Mã hóa bản rõ bằng Rail Fence Cipher. """
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def decrypt():
+    """ Giải mã bản mã bằng Rail Fence Cipher. """
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
 # main function
