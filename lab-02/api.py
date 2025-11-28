299e1df1a318d866b2ae086c28e164a8f5ff40b0
lynki1509 <lynki1509@gmail>
2025-11-28 15:43:51 +0700

    [add] lab-02 vigenere

---
+++ lab-02/api.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+from cipher.caesar import CaesarCipher
+from cipher.vigenere import VigenereCipher
+
+app = Flask(__name__)
+
+# CAESAR CIPHER ALGORITHM
+caesar_cipher = CaesarCipher()
+
+# VIGENERE CIPHER ALGORITHM
+vigenere_cipher = VigenereCipher()
+
+# ==================================
+# CAESAR CIPHER ENDPOINTS
+# ==================================
+
+@app.route("/api/caesar/encrypt", methods=["POST"])
+def caesar_encrypt():
+    """
+    Mã hóa bản rõ bằng Caesar Cipher.
+    Input: {"plain_text": "...", "key": "..."}
+    """
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+@app.route("/api/caesar/decrypt", methods=["POST"])
+def caesar_decrypt():
+    """
+    Giải mã bản mã bằng Caesar Cipher.
+    Input: {"cipher_text": "...", "key": "..."}
+    """
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+# ==================================
+# VIGENERE CIPHER ENDPOINTS
+# ==================================
+
+@app.route('/api/vigenere/encrypt', methods=['POST'])
+def vigenere_encrypt():
+    """
+    Mã hóa bản rõ bằng Vigenère Cipher.
+    Input: {"plain_text": "...", "key": "..."}
+    """
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/vigenere/decrypt', methods=['POST'])
+def vigenere_decrypt():
+    """
+    Giải mã bản mã bằng Vigenère Cipher.
+    Input: {"cipher_text": "...", "key": "..."}
+    """
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+# main function
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
